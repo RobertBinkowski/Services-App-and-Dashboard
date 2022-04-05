293e44876d9d405a225e1943d95083f1455390a4
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2022-04-05 22:26:28 +0100

    footer

--- services_app/resources/views/welcome.blade.php
+++ services_app/resources/views/welcome.blade.php
@@ -44,7 +44,30 @@
 
     </main>
     <footer>
+        //Create a static footer
 
+        <img src="#" alt="logo" class="nav-logo">
+
+        <br>
+        <p>Links</p>
+        <a href="https://robertbinkowski.me/" target="_blanc">
+            Website
+        </a>
+        <a href="https://docs.google.com/document/d/1SQ6XWF39Ibd1BbfVPTALshtNB7liDILF/view" target="_blanc">
+            Resume
+        </a>
+        <a href="https://www.linkedin.com/in/robert-binkowski-9bb565195/" target="_blanc">
+            LinkedIn
+        </a>
+        <a href="https://github.com/RobertBinkowski" target="_blanc">
+            GitHub
+        </a>
+
+        <p>All Rights Reserved to <a href="https://robertbinkowski.me/">Robert Binkowski</a> <br> 2022 -
+            <script>
+                document.write(/\d{4}/.exec(Date())[0])
+            </script>
+        </p>
     </footer>
 </body>
 
